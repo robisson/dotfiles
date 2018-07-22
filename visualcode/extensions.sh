9db823159a34dc9fbf427530b305d6e275982120
Robisson Oliveira <robisson@robissonoliveira.com.br>
2018-07-22 10:19:42 -0300

    Adding extensions to visual code config

---
+++ visualcode/extensions.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension EditorConfig.EditorConfig
+code --install-extension eg2.vscode-npm-script
+code --install-extension esbenp.prettier-vscode
+code --install-extension flowtype.flow-for-vscode
+code --install-extension mikestead.dotenv
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension ornithologist.ios-snippets
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension Pivotal.vscode-manifest-yaml
+code --install-extension redhat.java
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension robinbentley.sass-indented
+code --install-extension vsmobile.vscode-react-native
+code --install-extension xabikos.JavaScriptSnippets
+code --install-extension xabikos.ReactSnippets
